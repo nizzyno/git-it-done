ce20ed3dedb13e4f63758de417063c305aeb1e28
Noah <mcbeasty11.nh@icloud.com>
2021-03-26 00:35:56 -0500

    capture server response

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,27 @@
-const getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+const getUserRepos = function(user) {
+    // format the github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
 
-getUserRepos();
+// const getUserRepos = function () {
+//     fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+//         response.json().then(function (data) {
+//             console.log(data);
+//         });
+//     });
+// };
+    //     fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    //         console.log("inside", response);
+    //     });
+
+    //     console.log("outside");
+    // };
+
+    getUserRepos("nizzyno");
